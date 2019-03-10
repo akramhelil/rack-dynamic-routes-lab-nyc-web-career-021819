93bd7e31ccbcb34836b6c1b58c745d15e7bcc257
akramhelil <akramhelil@gmail.com>
2019-03-09 19:01:21 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,11 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      #item_name is an array of names that after split from the /items/
       if item = @@items.find {|t| t.name == item_name}
+        #item will find the first object that name matches with the item_name
         resp.write item.price
+        # return the price of the item object
       else
         resp.write "Item not found"
         resp.status = 400
